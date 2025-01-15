d9339026f3e01ae3632675e699b35bf0d08d312e
GUIBATISTA16 <guimcbatista@gmai.com>
2025-01-15 15:47:13 +0000

    Mini Bug fix

--- lib/widget/standalonewidgets/logout.dart
+++ lib/widget/standalonewidgets/logout.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shopping_list/riverpod/selectedlistprovider.dart';
 
 import '../../riverpod/loggeduserprovider.dart';
 import '../../screens/login.dart';
@@ -19,6 +20,7 @@ class Logout extends ConsumerWidget {
       onPressed: () async {
         await FirebaseAuth.instance.signOut();
         ref.read(userUidProvider.notifier).state = null;
+        ref.read(selectedListaNotifierProvider.notifier).selectLista(null);
 
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
